9748dd8cf92d0d550f6609d451fb6ea284bbea0a
Jesus Gastelum <gastelumtec21@gmail.com>
2023-09-01 11:33:55 -0700

    add Projects and Header components

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,49 @@
 import { defineCollection, z } from 'astro:content';
 
 const skillsCollection = defineCollection({
-    type: "data",
-    schema: z.object({
-        name: z.string(),
-        logo: z.string()
+  type: 'data',
+  schema: z.array(
+    z.object({
+      name: z.string(),
+      logo: z.string(),
     })
+  ),
+});
+
+const projectsCollection = defineCollection({
+  type: 'data',
+  schema: z.array(
+    z.object({
+      name: z.string(),
+      technologies: z.array(
+        z.object({
+          name: z.string(),
+          img: z.string(),
+        })
+      ),
+      description: z.string(),
+      fulldesc: z.string(),
+      img: z.string(),
+      otherImg: z.string(),
+      code: z.string(),
+      deploy: z.string(),
+    })
+  ),
+});
+
+const socialMediaCollection = defineCollection({
+  type: 'data',
+  schema: z.array(
+    z.object({
+      name: z.string(),
+      link: z.string(),
+      img: z.string(),
+    })
+  ),
 });
 
 export const collections = {
-    "skills": skillsCollection
-}
+  skills: skillsCollection,
+  projects: projectsCollection,
+  'social-media': socialMediaCollection,
+};
