f4237fe0bde88e0087ddc76f33083db45a24fc8b
Jesus Gastelum <gastelumtec21@gmail.com>
2023-09-02 11:46:25 -0700

    remove Skills section and move to aboutme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      colors: {
+        "background": "#f3f4f5",
+        "background-skills": "#273036"
+      },
       backgroundImage: {
         hero: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(0,0,0,0.7204131652661064) 100%), url(/img/hero.jpg)',
       },
